6790aca828d0ef0a1a93ee95d555344aa1796d7f
wwwchendev <wwwchendev@gmail.com>
2024-02-24 14:48:35 +0800

    新增scroll-to-top元件

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { Announcement, Navbar, ScrollToTop } from '@/components'
+import { useOffset } from '@/context/OffsetContext'
 
 function App() {
+  const { announcementVisible } = useOffset()
   return (
     <>
+      {announcementVisible && <Announcement />}
+      <Navbar />
       <Outlet />
+      <ScrollToTop />
     </>
   )
 }
