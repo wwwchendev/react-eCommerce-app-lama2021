da5749b46f65469320ab9b018cf49bb8568ff4d7
wwwchendev <wwwchendev@gmail.com>
2024-02-24 17:04:21 +0800

    設定單獨產品頁

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import { Announcement, Navbar, Footer, ScrollToTop } from '@/components'
 import { useOffset } from '@/context/OffsetContext'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 function App() {
   const { announcementVisible } = useOffset()
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }, [location.pathname])
   return (
     <>
       {announcementVisible && <Announcement />}
