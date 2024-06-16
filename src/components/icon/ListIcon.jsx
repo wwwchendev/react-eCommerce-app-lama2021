4f12a730e532b7bc17db74deffbe05f7fa568d14
wwwchendev <wwwchendev@gmail.com>
2024-06-16 13:21:41 +0800

    樣式調整

--- src/components/icon/ListIcon.jsx
+++ src/components/icon/ListIcon.jsx
@@ -1,6 +1,6 @@
 const ListIcon = ({ $color = '#333' }) => {
   return (
-    <svg viewBox='0 0 24 24' fill={$color} xmlns='http://www.w3.org/2000/svg'>
+    <svg viewBox='0 0 24 24' fill={'#333'} xmlns='http://www.w3.org/2000/svg'>
       <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
       <g
         id='SVGRepo_tracerCarrier'
@@ -11,7 +11,7 @@ const ListIcon = ({ $color = '#333' }) => {
         {' '}
         <path
           d='M8 6L21 6.00078M8 12L21 12.0008M8 18L21 18.0007M3 6.5H4V5.5H3V6.5ZM3 12.5H4V11.5H3V12.5ZM3 18.5H4V17.5H3V18.5Z'
-          stroke={$color}
+          stroke={'#333'}
           strokeWidth='2'
           strokeLinecap='round'
           strokeLinejoin='round'
